ed215c6042c8a0f39e0eade272db549f7166d9f0
Kelvinkimotho <kelvnkwaruingi@gmail.com>
2025-04-03 04:01:50 -0700

    Updated the js for teams

--- js/script.js
+++ js/script.js
@@ -4,4 +4,16 @@ function toggleMenu() {
     navLinks.classList.toggle("active");
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+    const teamContainer = document.querySelector(".team-container");
+    const prevBtn = document.querySelector(".prev-team");
+    const nextBtn = document.querySelector(".next-team");
 
+    nextBtn.addEventListener("click", () => {
+        teamContainer.scrollBy({ left: 250, behavior: "smooth" });
+    });
+
+    prevBtn.addEventListener("click", () => {
+        teamContainer.scrollBy({ left: -250, behavior: "smooth" });
+    });
+});
